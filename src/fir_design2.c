d8c96e0f788da6e6807e603114714544a9f2ba97
James Xie <115512123+CosmoJX@users.noreply.github.com>
2024-12-11 12:38:59 -0500

    Update fir_design2.c

--- src/fir_design2.c
+++ src/fir_design2.c
@@ -1,27 +1,24 @@
 #define N_TAPS 13
 #include "ac_int.h"
 
-typedef int coef_t;
-typedef int data_t;
-typedef int acc_t;
+typedef ac_int<8,false> coef_t;
+typedef ac_int<8,false> data_t;
+typedef ac_int<8,false> acc_t;
 
 void fir(data_t *y, data_t x) { 
-	coef_t c[N_TAPS] = {-86, 0, 62, 0, 400, 0, 999, 0, 400, 0, 62, 0, -86};
+	coef_t c[N_TAPS] = {1, 0, 5, 0, 10, 0, 15, 0, 10, 0, 5, 0, 1};
 	static data_t shift_reg[N_TAPS];
 	acc_t acc;
 	int i;
 	acc = 0;
 Shift_Accum_Loop:
-	for (i = N_TAPS-1; i >= 0; i--){
+	for (i = N_TAPS-1; i >= 0; --i){
 		if( i == 0 ){
-			acc += x * c[0];
-			shift_reg[0] = x;
+			shift_reg[i] = x;	
 		}else {
-			shift_reg[i] = shift_reg[i-1];
-			acc += shift_reg[i] * c[i];
+			shift_reg[i] = shift_reg[i-1];	
 		}
+		acc += shift_reg[i] * c[i];
 	}
 	*y = acc;
 }
-
-
