b187f52c126048600a86fec167af7cb79fa7cc77
Cameron Bedard <cameronobedard@gmail.com>
2024-12-11 15:10:09 -0500

    added module to project.v

--- src/project.v
+++ src/project.v
@@ -16,12 +16,20 @@ module tt_um_fir_filter2 (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  wire uo_uio_out = {uo_out, uio_out};
+
+  fir fir_filter( 
+    .clk(clk),              // Clock
+    .rst(~rst_n),           // Reset
+    .x_rsc_dat(ui_in),      // Input data
+    .y_rsc_dat(uo_uio_out), // Output data
+  );
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out  = uo_uio_out[15:8]; 
+  assign uio_out = uo_uio_out[7:0];
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, 1'b0, uio_oe};
 
 endmodule
