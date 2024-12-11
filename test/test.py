37eff453cd5652da5a7a7a80f8f0a54fda56bfb1
CosmoJX <hx2227@nyu.edu>
2024-12-11 14:45:19 -0500

    updated test.py

--- test/test.py
+++ test/test.py
@@ -25,16 +25,33 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # # Set the input values you want to test
+    # dut.ui_in.value = 20
+    # dut.uio_in.value = 30
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    # # Wait for one clock cycle to see the output values
+    # await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # # The following assersion is just an example of how to check the output values.
+    # # Change it to match the actual expected output of your module:
+    # assert dut.uo_out.value == 50
+
+    shift = [0]*13
+    for i in range(30):
+        dut.ui_in.value = i
+        await ClockCycles(dut.clk, 1)
+        assert ((dut.uo_out.value << 8) | dut.uio_out.value) == fir(shift, i)
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+
+def fir(shift, x):
+    coef = [1, 0, 10, 0, 20, 0, 50, 0, 20, 0, 10, 0, 1]
+    y = 0
+    for i in range(len(coef)):
+        if i == 0:
+            shift[i] = x
+        else:
+            shift[i] = shift[i-1]
+        y += shift[i]*coef[i]
+    return y
