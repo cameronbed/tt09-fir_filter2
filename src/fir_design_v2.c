f08e828207794922a57a82995164fe73e30b70b3
Cameron Bedard <cameronobedard@gmail.com>
2024-12-11 15:11:18 -0500

    formatted fir_design_v2.c and verilator in settings

--- src/fir_design_v2.c
+++ src/fir_design_v2.c
@@ -1,27 +1,31 @@
-/fir_filter/
-/
+// fir_filter/
+
 #define N_TAPS 13
 #include "ac_int.h"
 
-typedef ac_int<8,false> coef_t;
-typedef ac_int<8,false> data_t;
-typedef ac_int<16,false> acc_t;
+typedef ac_int<8, false> coef_t;
+typedef ac_int<8, false> data_t;
+typedef ac_int<16, false> acc_t;
 
-void fir(acc_t *y, data_t x) { 
+void fir(acc_t *y, data_t x)
+{
 	coef_t c[N_TAPS] = {1, 0, 10, 0, 20, 0, 50, 0, 20, 0, 10, 0, 1};
 	static data_t shift_reg[N_TAPS];
 	acc_t acc;
 	int i;
 	acc = 0;
 Shift_Accum_Loop:
-	for (i = N_TAPS-1; i >= 0; --i){
-		if( i == 0 ){
-			shift_reg[i] = x;	
-		}else {
-			shift_reg[i] = shift_reg[i-1];	
+	for (i = N_TAPS - 1; i >= 0; --i)
+	{
+		if (i == 0)
+		{
+			shift_reg[i] = x;
+		}
+		else
+		{
+			shift_reg[i] = shift_reg[i - 1];
 		}
 		acc += shift_reg[i] * c[i];
 	}
 	*y = acc;
 }
-
