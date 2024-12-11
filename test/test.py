8db9abacec8409e1e3f0aa58db05b427c1f5c28a
CosmoJX <hx2227@nyu.edu>
2024-12-11 16:38:29 -0500

    updated test.py

--- test/test.py
+++ test/test.py
@@ -39,7 +39,7 @@ async def test_project(dut):
     shift = [0]*13
     for i in range(30):
         dut.ui_in.value = i
-        await ClockCycles(dut.clk, 22)
+        await ClockCycles(dut.clk, 21)
         output = ((dut.uo_out.value << 8) | dut.uio_out.value)
         #await ClockCycles(dut.clk, 10)
         value = fir(shift, i)
